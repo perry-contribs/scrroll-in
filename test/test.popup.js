69a1ab8a942e9a58ad00e8419632377aaaf75e65
Parikshit Hooda <phooda804@live.com>
2019-10-10 02:53:48 +0530

    fix test.popup.js starter demo

--- test/test.popup.js
+++ test/test.popup.js
@@ -1,6 +1,6 @@
 var assert = chai.assert;
 describe('test1()', function() {
     it('should return 1', function() {
-        assert.equal([1, 2, 3].indexOf(4), -1);
+        assert.equal(, 1);
     });
 });
